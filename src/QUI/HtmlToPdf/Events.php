5b0dd568a39ae35a52cfc0dfce492ba0eff22029
Patrick Müller <p.mueller@pcsg.de>
2016-07-21 10:48:39 +0200

    feat: setup checkt, ob wkhtmltopdf vorhanden und ausführbar ist

---
+++ src/QUI/HtmlToPdf/Events.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file contains \QUI\HtmlToPdf\Events
+ */
+
+namespace QUI\HtmlToPdf;
+
+use QUI;
+
+/**
+ * Document that receives HTML and outputs PDF
+ *
+ * @author www.pcsg.de (Patrick Müller)
+ */
+class Events
+{
+    /**
+     * Event: onPackageSetup
+     *
+     * @param QUI\Package\Package $Package
+     * @throws QUI\Exception
+     */
+    public static function onPackageSetup($Package)
+    {
+        // check if wkhtmltopdf is executable
+        $wkhtmltopdfExecutable = dirname(dirname(dirname(dirname(__FILE__)))) . '/lib/wkhtmltopdf/bin/wkhtmltopdf';
+
+        if (!file_exists($wkhtmltopdfExecutable)) {
+            throw new QUI\Exception(array(
+                'pcsg/htmltopdf',
+                'exception.events.onpackagesetup.missing.wkhtmltopdf'
+            ));
+        }
+
+        if (!is_executable($wkhtmltopdfExecutable)) {
+            exec('chmod +x ' . $wkhtmltopdfExecutable, $output, $exitStatus);
+
+            if (!is_executable($wkhtmltopdfExecutable)) {
+                throw new QUI\Exception(array(
+                    'pcsg/htmltopdf',
+                    'exception.events.onpackagesetup.wkhtmltopdf.not.executable',
+                    array(
+                        'path' => $wkhtmltopdfExecutable
+                    )
+                ));
+            }
+        }
+
+
+    }
+}
