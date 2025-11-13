9b39cd6bda898e9890b266a765ff5a6f0a3e0786
Patrick Müller <p.mueller@pcsg.de>
2025-11-13 15:39:58 +0100

    chore: composer.json and .gitlab-ci.yaml fixes

--- src/QUI/HtmlToPdf/Events.php
+++ src/QUI/HtmlToPdf/Events.php
@@ -5,6 +5,7 @@ namespace QUI\HtmlToPdf;
 use QUI;
 use Smarty;
 use SmartyException;
+use QUI\Package\Package;
 
 /**
  * Document that receives HTML and outputs PDF
@@ -13,6 +14,26 @@ use SmartyException;
  */
 class Events
 {
+    public static function onPackageSetup(Package $package): void
+    {
+
+    }
+
+    private static function migrateConfigFromV3(Package $package): void
+    {
+        try {
+            $config = $package->getConfig();
+
+            if (is_null($config)) {
+                throw new QUI\Exception("Could not load config from {$package->getName()}.");
+            }
+
+            $convertExecutable = $config->get('')
+        } catch (\Exception $exception) {
+            QUI\System\Log::writeException($exception);
+        }
+    }
+
     /**
      * Register Smarty functions that are useful for HTML to PDF generation.
      *
