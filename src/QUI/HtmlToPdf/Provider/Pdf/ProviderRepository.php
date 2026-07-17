6e563bf482a213c30356def3a3c067489318c9ec
Patrick Müller <p.mueller@pcsg.de>
2026-07-17 09:11:55 +0200

    fix: resolve PHPStan provider repository errors

--- src/QUI/HtmlToPdf/Provider/Pdf/ProviderRepository.php
+++ src/QUI/HtmlToPdf/Provider/Pdf/ProviderRepository.php
@@ -3,7 +3,6 @@
 namespace QUI\HtmlToPdf\Provider\Pdf;
 
 use QUI;
-use Throwable;
 
 use function class_exists;
 use function is_a;
@@ -86,11 +85,7 @@ class ProviderRepository implements ProviderRepositoryInterface
                         continue;
                     }
 
-                    try {
-                        $providers[] = new $class();
-                    } catch (Throwable $e) {
-                        QUI\System\Log::writeException($e);
-                    }
+                    $providers[] = new $class();
                 }
             } catch (QUI\Exception $Exception) {
                 QUI\System\Log::writeException($Exception);
